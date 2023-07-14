fa81f0f074d4bfff4de40505d6d2a3ea74af3eb4
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-14 01:16:46 -0400

    prepared seeding for users

---
+++ seed.js
@@ -0,0 +1,16 @@
+const db = require('./db');
+const {User} = require ('./db/models');
+
+const seedUsers = [
+    {email: 'john.doe@gmail.com', password: 'tofu123', isAdmin: true},
+    {email: 'jane.doe@gmail.com', password: 'tomato456', isAdmin: false},
+    {email: 'lily.rose@gmail.com', password: 'sprinkles789', isAdmin: false},
+];
+
+
+const seed = async () => {
+    await User.bulkCreate(seedUsers);
+};
+
+//only seed once
+seed().then(() => process.exit());
