f473669d4e86efe3b9302db74898d3c827c64dbe
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-17 15:43:53 -0400

    add socket.io depedency

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http')
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const passport = require('passport');
