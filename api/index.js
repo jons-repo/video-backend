e04fa631393ffc9f85149185146fcbf558c56188
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-17 22:58:02 -0400

    added livestream routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 //Mounted on /api
 router.use('/user', require("./user"));
+router.use('/livestream', require("./livestream"));
 
 //404 Handling
 router.use((req, res, next) => {
