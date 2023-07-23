5e0b7e60131ae5b10e49822965f8028ddb0a8f45
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-07-23 17:48:40 -0400

    Add nodemailer function

---
+++ sendNotifications.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+async function sendEmailNotification(emails, subject, text) {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.elasticemail.com',
+            port: 2525,
+            auth: {
+                user: 's.jonathan4252@gmail.com',
+                pass: 'F20B35C1488975822809CAC73EEF648C1360', 
+            },
+        });
+
+        const mailOptions = {
+            from: 's.jonathan4252@gmail.com',
+            to: emails.join(','),
+            subject: subject,
+            text: text,
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        console.log('Email notification sent');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+}
+
+module.exports = sendEmailNotification;
